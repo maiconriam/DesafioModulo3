558121a7a15806632e817ed3e4b479f09e83c789
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-10-08 12:32:41 -0300

    Criar classe sistema

---
+++ src/br/com/zup/Sistema.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Sistema {
+    public static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+}
